dd7f28591b0fd9ae85934cad963e8fadc07390e0
Mahdi_B <62808493+mahdibaftiyan@users.noreply.github.com>
2022-09-24 16:06:18 +0330

    developer switch page

--- script/main.js
+++ script/main.js
@@ -1,8 +1,15 @@
 
+// bars code
 const item = document.querySelector(".link-item")
 const label = document.querySelector(".label")
 const navigation = document.querySelector("#navigation")
-const drop = document.querySelector('#drop').addEventListener('click', (e) => {
+
+
+document.querySelector('#drop').addEventListener('click', (e) => {
+    drop()
+})
+
+const drop = () => {
     if (item.style.visibility == "visible") {
         item.style.visibility = "hidden"
         item.style.transform = "translateY(-300px)"
@@ -16,6 +23,27 @@ const drop = document.querySelector('#drop').addEventListener('click', (e) => {
         item.style.boxShadow = "0px 1px 0px 0px #d3c8c878"
         label.innerHTML = '<i class="fa-solid fa-xmark"></i>'
     }
+}
 
-})
+const dropclose = () => {
+    item.style.visibility = "hidden"
+    item.style.transform = "translateY(-300px)"
+    item.style.boxShadow = "none"
+    navigation.style.height = "80px"
+    label.innerHTML = ' <i class="fa-solid fa-bars"></i>'
+}
 
+
+// switch page
+removeClass = () => {
+    document.querySelector('.Active').classList.remove('Active')
+}
+
+changePage = (a) => {
+    removeClass()
+    dropclose()
+    id = a.getAttribute("data-id")
+    const item = document.querySelector(id)
+    item.classList.add('Active')
+
+}
