e19f28ded0d2ef31eee78c0ec366cacfd49b5d4a
Mahdi_B <62808493+mahdibaftiyan@users.noreply.github.com>
2022-09-25 22:18:54 +0330

    codes media Desktop view complit header&footer

--- script/main.js
+++ script/main.js
@@ -4,33 +4,35 @@ const item = document.querySelector(".link-item")
 const label = document.querySelector(".label")
 const navigation = document.querySelector("#navigation")
 
+let Validity = false
 
-document.querySelector('#drop').addEventListener('click', (e) => {
+document.querySelector('#drop').addEventListener('change', (e) => {
     drop()
 })
 
 const drop = () => {
-    if (item.style.visibility == "visible") {
+    if (Validity) {
         item.style.visibility = "hidden"
         item.style.transform = "translateY(-300px)"
         item.style.boxShadow = "none"
         navigation.style.height = "80px"
         label.innerHTML = ' <i class="fa-solid fa-bars"></i>'
+        Validity = false
+
     } else {
         item.style.visibility = "visible"
         item.style.transform = "translateY(0px)"
         navigation.style.height = "auto"
         item.style.boxShadow = "0px 1px 0px 0px #d3c8c878"
         label.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+        Validity = true
     }
 }
 
 const dropclose = () => {
-    item.style.visibility = "hidden"
-    item.style.transform = "translateY(-300px)"
-    item.style.boxShadow = "none"
-    navigation.style.height = "80px"
-    label.innerHTML = ' <i class="fa-solid fa-bars"></i>'
+
+    Validity ? drop() : console.log('close')
+
 }
 
 
